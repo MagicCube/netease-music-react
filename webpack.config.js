b7c62f0ae530a6d3278aab49cf2928f745e5b6a9
Henry Li <henry1943@163.com>
2017-01-20 20:20:06 +0800

    Add vendor and scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
 const path = require('path');
 
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
   entry: {
-    nm: './client/index.jsx'
+    vendor: './client/vendor/index.js',
+    nm: './client/nm/index.jsx'
   },
   resolve: {
     extensions: ['.js', 'jsx']
   },
   output: {
-    filename: '[name].min.js',
+    filename: './js/[name].min.js',
     path: path.resolve(__dirname, './dist/assets'),
     publicPath: '/assets',
   },
@@ -19,6 +23,26 @@ module.exports = {
         path.resolve(__dirname, './client'),
       ],
       loader: 'babel-loader',
+    },
+    {
+      test: /\.scss$/,
+      loader: ExtractTextPlugin.extract({
+        fallbackLoader: 'style-loader',
+        loader: ['css-loader', 'sass-loader']
+      })
     }]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: './css/[name].min.css'
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery'
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: './js/vendor.min.js',
+      minChunks: Infinity
+    })
+  ]
 };
